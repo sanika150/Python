19774d7ab9e45680e118d568597122b44101ee37
Sanika Dhamnaskar <63234272+sanika150@users.noreply.github.com>
2026-08-21 14:06:48 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,33 @@
+#from sys import getsizeof
+
+No = 11
+a = 'A'
+b = True
+c = "Sanika"
+d = 34.6
+e = '2+6j'
+
+print(No)
+print (id(No))
+#print (getsizeof(No))
+print (dir(No))
+print (isinstance(No,int))
+print (help(No))
+print("hash")
+print (hash(No))
+'''
+print(a)
+print (id(a))
+
+print(b)
+print (id(b))
+
+print(c)
+print (id(c))
+
+print(d)
+print (id(d))
+
+print(e)
+print (id(e))'''
+
